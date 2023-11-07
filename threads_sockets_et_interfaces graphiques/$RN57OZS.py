4549e731fe3acf187ef1d4ca750e5071af5223a2
dsqf <sdgf>
2023-11-07 17:14:01 +0100

    heum

--- threads_sockets_et_interfaces graphiques/$RN57OZS.py
+++ threads_sockets_et_interfaces graphiques/$RN57OZS.py
@@ -1,18 +1,42 @@
 import socket
 
-host = 'localhost'
+host = '0.0.0.0'
 port = 12345
-
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((host, port))
 server_socket.listen(1)
 print(f"Le serveur écoute sur {host}:{port}")
-while True:
-    client_socket, client_address = server_socket.accept()
-    print(f"Connexion entrante de {client_address}")
-    data = client_socket.recv(1024).decode()
-    print(f"Message reçu du client : {data}")
-    response = "Message reçu avec succès!"
-    client_socket.send(response.encode())
-    client_socket.close()
+
+try:
+    def srv():
+        while True:
+            conn, address = server_socket.accept()
+            print(f"Connexion entrante de {conn}")
+            while True:
+                data = conn.recv(1024).decode()
+
+                if data.lower() == 'bye':
+                    bye = "ok bye"
+                    conn.send(bye.encode())
+                    print(f"Le client {conn} c'est deco")
+                    break
+
+                if data.lower() == 'arret':
+                    arret = "ok arret"
+                    conn.send(arret.encode())
+                    server_socket.close()
+                    print("arret du srv")
+                    break
+                response = "bien recu"
+                conn.send(response.encode())
+                print(f"Message du client : {data}")
+            conn.close()
+            if data.lower() == 'arret':
+                break
+except ConnectionAbortedError:
+    print("Le client a fait de la merde")
+except ConnectionResetError:
+    print("Le client a fait de la merde")
+finally:
+    srv()
--- threads_sockets_et_interfaces graphiques/ex_client.py
+++ threads_sockets_et_interfaces graphiques/ex_client.py
@@ -1,15 +1,18 @@
 import socket
 
-
-host = 'localhost'
+host = '127.0.0.1'
 port = 12345
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
 client_socket.connect((host, port))
-message = "bonsoir"
-client_socket.send(message.encode())
-data = client_socket.recv(1024).decode()
-print("Données reçues du serveur :", data)
 
-
+while True:
+    message = input("Ton message (ou 'bye' pour quitter) : ")
+    client_socket.send(message.encode())
+    reply = client_socket.recv(1024).decode()
+    print("Serveur :", reply)
+    if message.lower() == 'bye':
+        break
+    if message.lower() == 'arret':
+        break
+client_socket.close()
