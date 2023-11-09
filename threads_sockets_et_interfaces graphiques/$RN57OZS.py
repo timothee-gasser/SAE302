47b7461ce2d88ed14b72ae0c2454d2f1c1a83f7f
timothée <timothee.uha@gmail.com>
2023-11-09 15:27:58 +0100

    synchone_ok

--- threads_sockets_et_interfaces graphiques/$RN57OZS.py
+++ threads_sockets_et_interfaces graphiques/$RN57OZS.py
@@ -8,39 +8,26 @@ server_socket.bind((host, port))
 server_socket.listen(1)
 print(f"Le serveur écoute sur {host}:{port}")
 
+conn, address = server_socket.accept()
+print(f"Connexion entrante de {address}")
+
 while True:
-    conn, address = server_socket.accept()
-    print(f"Connexion entrante de {address}")
+    data = conn.recv(1024).decode()
 
-    try:
-        while True:
-            data = conn.recv(1024).decode()
-
-            if not data:
-                print(f" Le client {address} c'est barré")
-                break
-
-            if data.lower() == 'bye':
-                bye = "ok bye"
-                conn.send(bye.encode())
-                print(f"Le client {address} s'est deco")
-                break
-
-            if data.lower() == 'arret':
-                arret = "ok arret"
-                conn.send(arret.encode())
-                server_socket.close()
-                print("Arrêt du serveur")
-                break
-
-            response = "bien recu"
-            conn.send(response.encode())
-            print(f"Message du client : {data}")
-
-    except ConnectionResetError:
-        print(f"Le client {address} a fait de la merde")
-
-    conn.close()
-
-    if data.lower() == 'arret':
+    if not data:
+        print(f"Le client {address} s'est déconnecté")
         break
+
+    if data.lower() == 'bye':
+        bye = "ok bye"
+        conn.send(bye.encode())
+        print(f"Le client {address} s'est déconnecté")
+        break
+
+    response = "bien recu"
+    conn.send(response.encode())
+    print(f"Message du client : {data}")
+
+conn.close()
+server_socket.close()
+print("Arrêt du serveur")
